5476a69abd71415b6b0a5d1dbcd85e3df629714f
James Young <james.s.young@gmail.com>
2018-05-26 16:52:35 -0300

    join/confirm agreement

--- src/controllers/channel.js
+++ src/controllers/channel.js
@@ -7,6 +7,18 @@ module.exports.open = (req, res, next) => {
   util.response(Promise.resolve({ success: true }), 200, res, next);
 };
 
+module.exports.join = (req, res, next) => {
+  const channelData = req.swagger.params.body.value;
+  console.log('close channel', channelData);
+  util.response(Promise.resolve({ success: true }), 200, res, next);
+};
+
+module.exports.confirm = (req, res, next) => {
+  const channelData = req.swagger.params.body.value;
+  console.log('close channel', channelData);
+  util.response(Promise.resolve({ success: true }), 200, res, next);
+};
+
 module.exports.close = (req, res, next) => {
   const channelData = req.swagger.params.body.value;
   console.log('close channel', channelData);
